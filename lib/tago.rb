e8a753196b5a7834c47a0d23fa90b45266f345da
Ramzi Hemadou <ramzihemadou@gmail.com>
2025-12-08 23:38:01 +0100

    Refactor locale handling by embedding English locale data directly in the Float class

--- lib/tago.rb
+++ lib/tago.rb
@@ -4,7 +4,6 @@
 # SPDX-License-Identifier: MIT
 
 require 'time'
-require_relative 'locales/en'
 
 # A new method to print time as text.
 #
@@ -16,6 +15,19 @@ class Float
     s = self
     s = -s if s.negative?
     if args.include?(:pretty)
+      locales = {
+        en: {
+          numbers: { 0 => 'zero', 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six',
+                     7 => 'seven', 8 => 'eight', 9 => 'nine', 10 => 'ten' },
+          units: { microsecond: %w[microsecond microseconds], millisecond: %w[millisecond milliseconds],
+                   second: %w[second seconds], minute: %w[minute minutes], hour: %w[hour hours], day: %w[day days],
+                   week: %w[week weeks] },
+          short_units: {
+            microsecond: 'μs', millisecond: 'ms', second: 'sec', minute: 'min', hour: 'hr', day: 'd', week: 'wk'
+          }
+        }
+      }.freeze
+
       if s < 0.001
         val = (s * 1_000_000).to_i
         unit = :microsecond
@@ -39,8 +51,11 @@ class Float
         unit = :week
       end
       short = args.include?(:short)
-      num = short ? val : Locales::EN.number_to_words(val)
-      unit_name = Locales::EN.unit_name(unit, val, short:)
+      number_to_words = val <= 10 ? locales[:en][:numbers][val] : val.to_s
+      num = short ? val : number_to_words
+      names = locales[:en][:units][unit]
+      unit_symbol = val == 1 ? names[0] : names[1]
+      unit_name = short ? locales[:en][:short_units][unit] : unit_symbol
       return format('%<num>s %<unit_name>s', num:, unit_name:)
     end
 
